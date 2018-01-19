5c4e54742bdaf846ca21eaa8d04f40e876ffb1d0
Spencer Wahl <spencer.wahl2@canada.ca>
2018-01-19 09:57:38 -0500

    add map load, add sample config

--- src/components/map-view.vue
+++ src/components/map-view.vue
@@ -1,8 +1,6 @@
 <template>
     <aside class="root">
-        <div>
-            <img src="https://i.imgur.com/kuV4Zdc.png" alt="" srcset="">
-        </div>
+        <div id="map-anchor" class="container"></div>
     </aside>
 </template>
 
@@ -12,12 +10,21 @@ import { Vue, Component, Watch, Prop, Inject } from 'vue-property-decorator';
 import api from './../api/main';
 
 @Component
-export default class MapView extends Vue {}
+export default class MapView extends Vue {
+    async mounted(): Promise<void>{
+        if ((<any>window).RZ) {
+            new (<any>window).RZ.Map(document.getElementById('map-anchor'), '../static/configs/config-ahccd-demo.en-CA.json');
+        }
+    }
+}
 </script>
 
 
 <style lang="scss" scoped>
 img {
     width: 100%;
+}
+#map-anchor {
+    height:500px;
 }
 </style>
