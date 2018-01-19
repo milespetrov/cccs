2beb64085703c02d87a4ec6673f8d10d8f3e4ae8
Spencer Wahl <spencer.wahl2@canada.ca>
2018-01-19 10:11:13 -0500

    add api tooltips, no template switching yet

--- src/components/map-view.vue
+++ src/components/map-view.vue
@@ -8,12 +8,27 @@
 import { Vue, Component, Watch, Prop, Inject } from 'vue-property-decorator';
 
 import api from './../api/main';
+import { EventEmitter } from 'events';
+import { ExtendedAPIPlugin } from 'webpack';
 
 @Component
 export default class MapView extends Vue {
     async mounted(): Promise<void>{
-        if ((<any>window).RZ) {
-            new (<any>window).RZ.Map(document.getElementById('map-anchor'), '../static/configs/config-ahccd-demo.en-CA.json');
+        let RZ = (<any>window).RZ;
+        if (RZ) {
+            new RZ.Map(document.getElementById('map-anchor'), '../static/configs/config-ahccd-demo.en-CA.json');
+            let tooltip;
+            RZ.mapAdded.subscribe( () => {
+                RZ.mapInstances[RZ.mapInstances.length - 1].ui.tooltip.mouseOver.subscribe(function (z: any) {
+                    z.event.preventDefault();
+                    z.attribs.then(function (a: any) {
+                        let name = a.station_name_nom;
+                        let value = a.Annual_Annuel;
+                        let currentTemplate = "<div class=' rv-tooltip-content'><span class='rv-tooltip-text'>Station: %(name)s<br />Trend Value: %(value)s</span></div>";
+                        let tooltip = z.add((<any>window).sprintf(currentTemplate, {name, value}));
+                    });
+                });
+            });
         }
     }
 }
