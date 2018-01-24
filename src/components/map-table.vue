3fa268b98434088cec20e7e4b93afbd39eb30758
Spencer Wahl <spencer.wahl2@canada.ca>
2018-01-24 11:38:14 -0500

    move map table to own component, allow reconstruction

---
+++ src/components/map-table.vue
@@ -0,0 +1,29 @@
+<template>
+    <details>
+        <summary>View map data</summary>
+            <div class="summary">
+                <table id="map-table" class="wb-tables table table-striped" data-wb-tables='{ "scrollX": true }' v-if="config">
+                    <thead>
+                        <tr>
+                            <th v-for="col in config.columns" :key="col.name">{{col.name}}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(feature, index) in config.data" :key="`${index}-feature`">
+                            <td v-for="(col,index) in config.columns" :key="`${index}-data`">{{feature[col.dataKey] || '-'}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div> 
+    </details>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Watch, Prop, Inject } from 'vue-property-decorator';
+
+@Component
+export default class MapTable extends Vue {
+    @Prop() config: any;
+}
+</script>
+
--- src/components/map-view.vue
+++ src/components/map-view.vue
@@ -1,57 +1,50 @@
 <template>
     <aside class="root">
         <div id="map-anchor"></div>
-        <details>
-            <summary>View map data</summary>
-            <div class="summary">
-                <table id="map-table" class="wb-tables table table-striped" data-wb-tables='{ "scrollX": true }' :config="config">
-                    <thead>
-                        <tr>
-                            <th v-for="col in config.columns" :key="col.name">{{col.name}}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(feature, index) in config.data" :key="`${index}-feature`">
-                            <td v-for="(col,index) in config.columns" :key="`${index}-data`">{{feature[col.data] || '-'}}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </details>
+        <map-table :key="currentVariable" :config="config"></map-table>
     </aside>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Watch, Prop, Inject } from 'vue-property-decorator';
-import { sprintf } from 'sprintf-js';
 
 import api from './../api/main';
+import { rGetCurrentVariable } from '../store/modules/app/index';
+import MapTable from './map-table.vue';
 
-@Component
+@Component({
+    components: {
+        'map-table': MapTable
+    }
+})
 export default class MapView extends Vue {
-    data: any;
+    updates:number = 0;
     config:any = {};
 
-    async mounted(): Promise<void> {
-        if (!this.data) {
-            this.data = await $.getJSON('http://cipgis.canadaeast.cloudapp.azure.com/arcgis/rest/services/AHCCD_Denorm/MapServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&f=json', (data) => {return data});
-
-            this.updateTable(this.data);
-        }
+    mounted(): void {
+        this.onVariableChange();
     }
 
-    updateTable(data:any): void {
+    get currentVariable(){
+        return rGetCurrentVariable(this.$store);
+    }
+
+    @Watch("currentVariable")
+    async onVariableChange() {
+        console.log('CHANGED');
+        let tempData: any = await $.getJSON('http://cipgis.canadaeast.cloudapp.azure.com/arcgis/rest/services/AHCCD_Denorm/MapServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&f=json', (data) => {return data});
         const columns: object[] = [];
         let tableData: string[];
 
-        Object.keys(data.features[0].attributes).forEach(column => {
+        Object.keys(tempData.features[0].attributes).forEach(column => {
            columns.push({
                name: column,
-               data: column
+               dataKey: column
            });
         });
 
-        tableData = (<object[]>data.features).map(feature => {
+        tableData = (<object[]>tempData.features).map(feature => {
+            
             return (<any>feature).attributes;
         });
 
