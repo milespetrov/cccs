0dd9061b0fd8252f5b4727afa50c6f8d1577640a
Spencer Wahl <spencer.wahl2@canada.ca>
2018-01-24 11:53:42 -0500

    undo removal of commented out code

--- src/components/map-view.vue
+++ src/components/map-view.vue
@@ -23,6 +23,40 @@ export default class MapView extends Vue {
 
     mounted(): void {
         this.onVariableChange();
+
+        // TODO: map disabled until the jquery collisions are fixed
+        /* let RZ = (<any>window).RZ;
+
+        if (!RZ) {
+            return;
+        }
+
+        console.log('what??');
+
+        new RZ.Map(
+            document.getElementById('map-anchor'),
+            '../static/configs/config-ahccd-demo.en-CA.json'
+        );
+
+        let tooltip;
+        RZ.mapAdded.subscribe(() => {
+            RZ.mapInstances[
+                RZ.mapInstances.length - 1
+            ].ui.tooltip.mouseOver.subscribe((z: any) => {
+                z.event.preventDefault();
+                z.attribs.then((a: any) => {
+                    let name = a.station_name_nom;
+                    let value = a.Annual_Annuel;
+                    let currentTemplate = `<div class=' rv-tooltip-content'><span class='rv-tooltip-text'>Station: %(name)s<br />Trend Value: %(value)s</span></div>`;
+                    let tooltip = z.add(
+                        sprintf(currentTemplate, {
+                            name,
+                            value
+                        })
+                    );
+                });
+            });
+        }); */
     }
 
     get currentVariable(){
