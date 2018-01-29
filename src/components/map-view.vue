f46211c4e11d9da8c44b4309779b4c73129fec46
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2018-01-29 07:40:36 -0500

    prevent map updates when not active

--- src/components/map-view.vue
+++ src/components/map-view.vue
@@ -1,9 +1,9 @@
 <template>
     <aside class="root">
 
-        <map-instance :key="`instance-${currentVariable}`"></map-instance>
+        <map-instance :key="`instance-${reloadKey}`"></map-instance>
 
-        <map-table :key="`table-${currentVariable}`" :config="config"></map-table>
+        <map-table :key="`table-${reloadKey}`" :config="config"></map-table>
 
        <!--  <rv-map class="myMap" id="mobile-map" is="rv-map"
             rv-config="http://fgpv.cloudapp.net/demo/develop/dev/samples/config/config-sample-01-structured-visibility-sets.json"
@@ -19,6 +19,7 @@ import { rVariableId } from '../store/modules/app/index';
 import MapTable from './map-table.vue';
 import MapInstance from './map-instance.vue';
 import sprintf from 'sprintf-js';
+import { log } from 'util';
 
 @Component({
     components: {
@@ -28,6 +29,38 @@ import sprintf from 'sprintf-js';
 })
 export default class MapView extends Vue {
     config: any = {};
+
+    /**
+     * The map and table components will be force-reloaded on the `reloadKey` change.
+     */
+    reloadKey: string = '';
+
+    /**
+     * Keeps track of the internal state of the view. When `false`, no update should be initiated inside the component.
+     */
+
+    isActive: boolean = false;
+
+    /**
+     * `Activated` lifecycle hook.
+     * https://vuejs.org/v2/api/#Options-Lifecycle-Hooks
+     */
+    activated(): void {
+        this.isActive = true;
+
+        // trigger data update change
+        this.onVariableChange();
+    }
+
+    /**
+     * `Activated` lifecycle hook.
+     * https://vuejs.org/v2/api/#Options-Lifecycle-Hooks
+     */
+    deactivated(): void {
+        console.log('activated', false);
+
+        this.isActive = false;
+    }
 
     mounted(): void {
         this.onVariableChange();
@@ -39,6 +72,15 @@ export default class MapView extends Vue {
 
     @Watch('currentVariable')
     async onVariableChange() {
+        // do not process any data updates when deactivated
+        if (!this.isActive) {
+            return;
+        }
+
+        // HACK: map is force-reloaded for the momement until its API supports adding/removing layers
+        // update the reload keys to force-reload the map and table components
+        this.reloadKey = this.currentVariable;
+
         const tempData: any = await $.getJSON(
             'http://cipgis.canadaeast.cloudapp.azure.com/arcgis/rest/services/AHCCD_Denorm/MapServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&f=json',
             data => {
