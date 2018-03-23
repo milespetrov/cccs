97912f8c2e0acb23fe8eb123d347ea18daa27a7c
Spencer Wahl <spencer.wahl2@canada.ca>
2018-03-23 14:01:15 -0400

    fix rebasing issue

--- src/api/cmip5-api.ts
+++ src/api/cmip5-api.ts
@@ -1,15 +1,9 @@
 import { DatasetApi } from './types';
 
 function getData(timePeriod: string, variable: string, featureId: string): Promise<any[]> {
-<<<<<<< HEAD
     const promise = new Promise<any[]>((resolve, reject) =>
         $.getJSON('./assets/configs/sfcWindSample.json', (data: any[]) => resolve(data))
     );
-=======
-    const promise = new Promise<any[]>((resolve, reject) => {
-        $.getJSON('./assets/configs/sfcWindSample.json', (data: any[]) => resolve(data));
-    });
->>>>>>> add time-slider tests for keypresses and clicks
 
     return promise;
 }
