87388995ead234d8ea66d20aa7367057e3b07062
Spencer Wahl <spencer.wahl2@canada.ca>
2018-05-31 11:58:34 -0400

    Revert "remove dataset selector"
    This reverts commit 464e34120011a1a2f715079cd24cbd8e9d985130.

--- src/configs/selectors/index.ts
+++ src/configs/selectors/index.ts
@@ -1,8 +1,9 @@
 import { VisualizationControlType } from '@/types';
 
 export * from './types';
+export * from './dataset-selector-config';
 export * from './rcp-selector-config';
 export * from './timeperiod-selector-config';
 export * from './variable-selector-config';
 
-export const defaultSelectors = [VisualizationControlType.Variable];
+export const defaultSelectors = [VisualizationControlType.Variable, VisualizationControlType.Dataset];
