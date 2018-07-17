11477a2684108ebfdb2de7bdd23d8bd1d1a12bf7
Spencer Wahl <spencer.wahl2@canada.ca>
2018-07-17 08:47:55 -0400

    single ramp config and clean up

--- assets/templates/ahccd/variables-script.js
+++ assets/templates/ahccd/variables-script.js
@@ -6,44 +6,44 @@ function parser(data, lang) {
             measurementType: {
                 title: 'Measurement type',
                 total_precip: 'Total precipitation',
-                rain: 'Rain ????',
+                //rain: 'Rain ????',
                 temp_mean: 'Mean temperature',
                 temp_max: 'Maximum temperature',
                 temp_min: 'Minimum temperature',
-                snow: 'Snow depth',
+                //snow: 'Snow depth',
                 wind_speed: 'Wind speed',
                 pressure_station: 'Surface pressure',
                 pressure_sea_level: 'Sea level pressure'
             },
             measurementUnit: {
                 total_precip: 'mm',
-                rain: 'mm ????',
+                //rain: 'mm ????',
                 temp_mean: '°C',
                 temp_max: '°C',
                 temp_min: '°C',
-                snow: 'mm ??',
-                wind_speed: 'm/s ??',
-                pressure_station: 'kPa ??',
-                pressure_sea_level: 'kPa ??'
+                //snow: 'mm ??',
+                wind_speed: 'km/h',
+                pressure_station: 'kPa',
+                pressure_sea_level: 'kPa'
             },
             timePeriod: {
                 title: 'Time period',
-                Jan: 'January ',
-                Feb: 'February ',
-                Mar: 'March ',
-                Apr: 'April ',
-                May: 'May ',
-                Jun: 'June ',
-                Jul: 'July ',
-                Aug: 'August ',
-                Sep: 'September ',
-                Oct: 'October ',
-                Nov: 'November ',
-                Dec: 'December ',
-                Win: 'Winter ',
-                Spr: 'Spring ',
-                Sum: 'Summer ',
-                Fal: 'Autumn ',
+                Jan: 'January',
+                Feb: 'February',
+                Mar: 'March',
+                Apr: 'April',
+                May: 'May',
+                Jun: 'June',
+                Jul: 'July',
+                Aug: 'August',
+                Sep: 'September',
+                Oct: 'October',
+                Nov: 'November',
+                Dec: 'December',
+                Win: 'Winter',
+                Spr: 'Spring',
+                Sum: 'Summer',
+                Fal: 'Autumn',
                 Ann: 'Annual'
             }
         },
@@ -51,23 +51,11 @@ function parser(data, lang) {
         'fr-CA': {}
     };
 
-    const sampleData = {
-        properties: {
-            trend_value: 237.4600067138672,
-            measurement_type: 'total_precip',
-            identifier: '1017230.Ann.total_precip',
-            period: 'Ann',
-            station_id: '1017230'
-        }
-    };
-
     console.log(data, lang);
 
     data.properties = {};
 
     data.forEach(({ key, value }) => (data.properties[key] = value));
-    // TODO: use sample data until ahccd wfs is fixed
-    // data = sampleData;
     data.tt = TRANSLATIONS[lang];
 
     return data;
