94dae9d9b15bb1adc389cbe1db5d43445090a61b
Spencer Wahl <spencer.wahl2@canada.ca>
2018-07-20 11:27:18 -0400

    hydro templates

---
+++ assets/templates/hydro/stations-script.js
@@ -0,0 +1,44 @@
+function parser(data, lang) {
+    const TRANSLATIONS = {
+        'en-CA': {
+            station_name: 'Station Name',
+            station_number: 'Station Number',
+            contributor: 'Contributor',
+            vertical_datum: 'Vertical Datum',
+            status: 'Status',
+            province: {
+                title: 'Province',
+                AB: 'Alberta',
+                BC: 'British Columbia',
+                MB: 'Manitoba',
+                NB: 'New Brunswick',
+                NL: 'Newfoundland and Labrador',
+                NS: 'Nova Scotia',
+                NT: 'Northwest Territories',
+                NU: 'Nunavut',
+                ON: 'Ontario',
+                PE: 'Prince Edward Island',
+                QC: 'Quebec',
+                SK: 'Saskatchewan',
+                YT: 'Yukon'
+            }
+        },
+
+        'fr-CA': {}
+    };
+
+    data.properties = {};
+
+    data.forEach(({ key, value }) => {
+        if (typeof value === 'string') {
+            value = value.trim();
+        }
+        data.properties[key] = value;
+    });
+
+    data.lang = lang;
+
+    data.tt = TRANSLATIONS[lang];
+
+    return data;
+}
