30c45473d9a84ed31bd4709647341b8fb7ed06dc
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2018-07-23 14:54:06 -0400

    remove old file

--- assets/templates/test-script.js
+++
@@ -1,9 +0,0 @@
-function parser(stuff) {
-    return stuff;
-
-    /* return stuff.split('\n').reduce((map, line) => {
-        const [key, value] = line.split('=');
-        map[(key || '').trim()] = (value || '').trim().replace(`'`, '');
-        return map;
-    }, {}); */
-}
