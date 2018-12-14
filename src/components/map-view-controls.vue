ca1aae18a1a06b6c440489182b15c835d34231ce
Spencer Wahl <spencer.wahl2@canada.ca>
2018-12-14 11:44:03 -0500

    activate export for rdpa/capa

--- src/components/map-view-controls.vue
+++ src/components/map-view-controls.vue
@@ -1,18 +1,16 @@
 <template>
     <div class="cip-view-controls">
-
-        <button
-            @click="showCollapse = !showCollapse"
-            class="cip-controls-toggle btn btn-primary hidden-md hidden-lg">
-
-            <span v-show="showCollapse"><font-awesome-icon icon="times" fixed-width /></span>
-            <span v-show="!showCollapse"><font-awesome-icon icon="sliders-h" fixed-width /></span>
+        <button @click="showCollapse = !showCollapse" class="cip-controls-toggle btn btn-primary hidden-md hidden-lg">
+            <span v-show="showCollapse">
+                <font-awesome-icon icon="times" fixed-width/>
+            </span>
+            <span v-show="!showCollapse">
+                <font-awesome-icon icon="sliders-h" fixed-width/>
+            </span>
             <span class="cip-label">{{ $t('settings.title') }}</span>
-
         </button>
 
         <b-collapse class="cip-controls-wrapper" v-model="showCollapse" id="cip-view-controls-collapse">
-
             <div class="cip-controls">
                 <div class="menu-option" v-for="controlRef in getControls" :key="`${controlRef}`">
                     <component :is="controlRef"></component>
@@ -21,7 +19,6 @@
                 <span class="separator hidden-sm hidden-xs"></span>
 
                 <div class="menu-option">
-
                     <b-dropdown variant="light" right class="cip-dropdown-right cip-selector" no-flip>
                         <template slot="button-content">
                             <div class="cip-content-wrap">
@@ -34,14 +31,16 @@
                             <div class="cip-dropdown-description">{{ $t(`${tDSPath}.description`) }}</div>
                         </div>
 
-                        <b-dropdown-divider></b-dropdown-divider> -->
-
-                        <!-- TODO: remove `v-if="datasetId !== 'capa'"` after upgrading to RAMP local printing; http://tfs.int.ec.gc.ca:8080/tfs/DC/CCCS_Viewer/_workitems?_a=edit&id=46168 -->
-                        <div role="group" aria-lableledby="map-image-export" :key="`group-a`" v-if="datasetId !== 'capa'">
+                        <b-dropdown-divider></b-dropdown-divider>-->
+                        <div role="group" aria-lableledby="map-image-export" :key="`group-a`">
                             <b-dropdown-header id="map-image-export">{{ $t(`${tDSPath}.mapImage_group`) }}</b-dropdown-header>
 
-                            <b-dropdown-item-button @click="downloadImage('png', true)">{{ $t(`${tDSPath}.png.fullName`) }}</b-dropdown-item-button>
-                            <b-dropdown-item-button @click="downloadImage('jpg', true)">{{ $t(`${tDSPath}.jpeg.fullName`) }}</b-dropdown-item-button>
+                            <b-dropdown-item-button
+                                @click="downloadImage('png', true)"
+                            >{{ $t(`${tDSPath}.png.fullName`) }}</b-dropdown-item-button>
+                            <b-dropdown-item-button
+                                @click="downloadImage('jpg', true)"
+                            >{{ $t(`${tDSPath}.jpeg.fullName`) }}</b-dropdown-item-button>
                         </div>
 
                         <b-dropdown-divider v-if="datasetId !== 'capa'"></b-dropdown-divider>
@@ -49,33 +48,35 @@
                         <div role="group" aria-lableledby="map-data-export" :key="`group-b`">
                             <b-dropdown-header id="map-data-export">{{ $t(`${tDSPath}.dataset_group`) }}</b-dropdown-header>
 
-                            <b-dropdown-item class="cip-nowrap" target="_blank" :href="`${dataCatalogueUrl}${metadataUUID}`">
-
+                            <b-dropdown-item
+                                class="cip-nowrap"
+                                target="_blank"
+                                :href="`${dataCatalogueUrl}${metadataUUID}`"
+                            >
                                 <i18n :path="`${tDSPath}.dataCatalogue.fullName`" tag="span" class="cip-name">
                                     <span class="wb-inv">{{ $t(`${tDSPath}.dataCatalogue.access`) }}</span>
                                 </i18n>
 
-                                <font-awesome-icon icon="external-link-alt" />
-                                
+                                <font-awesome-icon icon="external-link-alt"/>
                             </b-dropdown-item>
 
-                            <b-dropdown-item class="cip-nowrap" target="_blank" :href="`${dataQueryUrl}${queryToolRoute}`">
-
+                            <b-dropdown-item
+                                class="cip-nowrap"
+                                target="_blank"
+                                :href="`${dataQueryUrl}${queryToolRoute}`"
+                            >
                                 <i18n :path="`${tDSPath}.queryTool.fullName`" tag="span" class="cip-name">
                                     <span class="wb-inv">{{ $t(`${tDSPath}.queryTool.access`) }}</span>
                                 </i18n>
 
-                                <font-awesome-icon icon="external-link-alt" />
+                                <font-awesome-icon icon="external-link-alt"/>
                             </b-dropdown-item>
                         </div>
-
                     </b-dropdown>
                 </div>
             </div>
-
         </b-collapse>
     </div>
-
 </template>
 
 <script lang="ts">
@@ -139,6 +140,6 @@ export default class MapViewControls extends Vue {
 </script>
 
 <style lang="scss" scoped>
-@import './../styles/variables.scss';
-@import './../styles/view-controls.scss';
+@import "./../styles/variables.scss";
+@import "./../styles/view-controls.scss";
 </style>
