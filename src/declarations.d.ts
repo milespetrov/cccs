3ed4402351c150b8bed51b2fa0e9f7ef78aa8c89
Spencer Wahl <spencer.wahl2@canada.ca>
2019-03-11 12:59:48 -0400

    fix screenreader accessibility for sliders

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -7,3 +7,4 @@ declare module 'bootstrap-vue/es/components/dropdown';
 declare module 'bootstrap-vue/es/components/collapse';
 declare module 'bootstrap-vue/es/components/form-select';
 declare module '@fortawesome/vue-fontawesome';
+declare module 'nouislider';
